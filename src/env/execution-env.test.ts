1d5057fd9d219fd7002da744dc8b5ef7ddc08020
electrovir <electrovir@users.noreply.github.com>
2023-10-03 13:51:44 +0000

    Deploying to gh-pages from @ electrovir/sentry-vir@6b3885e5dfa615e7eb289fb6dd579978ff426420 🚀

--- src/env/execution-env.test.ts
+++
@@ -1,9 +0,0 @@
-import {getEnumTypedValues} from '@augment-vir/common';
-import {assert} from '@open-wc/testing';
-import {SentryExecutionEnvEnum, sentryDepByEnv} from './execution-env';
-
-describe('sentryDepByEnv', () => {
-    it('includes an entry for each execution env', () => {
-        assert.hasAllKeys(sentryDepByEnv, getEnumTypedValues(SentryExecutionEnvEnum));
-    });
-});
