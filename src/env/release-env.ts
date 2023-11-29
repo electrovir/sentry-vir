47bae49f76c3ece69657bd06e786def82fe711fd
electrovir <electrovir@users.noreply.github.com>
2023-11-29 20:25:22 +0000

    [major] remove SentryReleaseEnvEnum

--- src/env/release-env.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Used to determine logging behavior (dev does not send events to Sentry but still logs them
- * locally).
- */
-export enum SentryReleaseEnvEnum {
-    Prod = 'prod',
-    Dev = 'dev',
-}
