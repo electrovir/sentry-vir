74145c6d0b3f62f1dc71e488e2438a1d03270aac
electrovir <electrovir@users.noreply.github.com>
2025-11-01 14:08:30 +0000

    [patch] fix deeply nested objects not logging correctly

--- src/init-sentry/sentry-config.ts
+++ src/init-sentry/sentry-config.ts
@@ -69,6 +69,7 @@ const sentryConfigByEnv = {
                 BrowserSentry.functionToStringIntegration(),
                 BrowserSentry.globalHandlersIntegration(),
             ],
+            normalizeDepth: 100,
         };
 
         return options;
@@ -82,6 +83,7 @@ const sentryConfigByEnv = {
                 NodeSentry.nodeContextIntegration(),
                 NodeSentry.functionToStringIntegration(),
             ],
+            normalizeDepth: 100,
         };
 
         return options;
